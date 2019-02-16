bce0d900e02f46768fbbcefdaf7f10bbaf2ff0b9
Lawrence Spector <lawrence.spector@gmail.com>
2019-02-16 12:20:06 -0500

    Update build process

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+set -eou pipefail
+
 mkdir -p bin/Debug
 pushd bin/Debug
 
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug ../.. && make -j8
+
+bin/cppinvert_test
 
 popd
 
